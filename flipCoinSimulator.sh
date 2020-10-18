f3956ef8cb49ee5a5e4518dc8761309727066462
AkashSuchak <suchak.akash@gmail.com>
2020-10-18 21:26:12 +0530

    Extend Uc3 : Game Continues till the difference of Minimum 2 Points is achieved

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,37 +1,45 @@
-#! /bin/bash
+#! /bin/bash 
 
 #Author : Akash Suchak
-#Flipping Coin till either of them have won 21 times and Showing Winner Wih How Much
+#Extend UC 3  to ensure if its tie then the game continues till the difference of minimum 2 points is Achieved
 
 #Variables to take total value
 totalHeads=0
 totalTails=0
 
-#Loop till Heads or Tails will not reach at 21
-while [ "$totalHeads" -lt "21" -a "$totalTails" -lt "21" ]
-do
-	#Random Value 0 or 1
-	isCheck=$((RANDOM%2))
+#Fuction to Check number occurence of Heads and tails
+function flipCoins(){
 
-	#Checking Heads or Tails
-	if [ "$isCheck" -eq 0 ]; then
-		totalHeads=$(($totalHeads + 1))
-	else
-		totalTails=$(($totalTails + 1))
-	fi
+	#Random Value 0 or 1
+        isCheck=$((RANDOM%2))
+
+        #Checking Heads or Tails
+        if [ "$isCheck" -eq 0 ]; then
+                totalHeads=$(($totalHeads + 1))
+        else
+                totalTails=$(($totalTails + 1))
+        fi
+
+}
+
+#Loop till Heads or Tails will not reach at 21 And Get Difference Atleast by 2
+while [ "$totalHeads" -lt 21 -a "$totalTails" -lt 21 -o $(("$totalHeads" - "$totalTails")) -lt 2 ]
+do
+	#calling Function
+	flipCoins
 done
 
 #Display Results
 echo "Heads got $totalHeads : Tails got $totalTails"
 
-#Checking Who is winner and By howw much
+#Checking Who is winner and By how much
 if [ "$totalHeads" -gt "$totalTails" ]; then
 	wonBy=$(( "$totalHeads" - "$totalTails"))
-	echo "Head Won by : $wonBy"
+	echo "Heads Won by : $wonBy"
 
 elif [ "$totalHeads" -lt "$totalTails" ]; then
         wonBy=$(( "$totalTails" - "$totalHeads"))
-        echo "Tail Won by : $wonBy"
+        echo "Tails Won by : $wonBy"
 
 elif [ "$totalHeads" -eq "$totalTails" ]; then
         echo "Oops ! Its Tie "
